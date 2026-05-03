c3cb031d246a60af4422620d78f0cceaedf915b0
MarsMyth <callumshelton2503@gmail.com>
2026-05-03 16:10:37 +1000

    Initial Commit

---
+++ src/main/java/org/mythic_goose/challenge_series/ChallengeSeriesAPIDataGenerator.java
@@ -0,0 +1,11 @@
+package org.mythic_goose.challenge_series;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChallengeSeriesAPIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
