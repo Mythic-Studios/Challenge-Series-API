67b2816330c43e706498cd7b896acca957536d5b
MarsMyth <callumshelton2503@gmail.com>
2026-05-04 08:07:06 +1000

    1.1.1 - Creative Mode Tab init

--- src/main/java/org/mythic_goose/challenge_series/ChallengeSeriesAPI.java
+++ src/main/java/org/mythic_goose/challenge_series/ChallengeSeriesAPI.java
@@ -2,23 +2,16 @@ package org.mythic_goose.challenge_series;
 
 import net.fabricmc.api.ModInitializer;
 
+import org.mythic_goose.challenge_series.creative_tab_v1.ModCreativeTabs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ChallengeSeriesAPI implements ModInitializer {
 	public static final String MOD_ID = "challenge_series";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModCreativeTabs.init();
 	}
 }
