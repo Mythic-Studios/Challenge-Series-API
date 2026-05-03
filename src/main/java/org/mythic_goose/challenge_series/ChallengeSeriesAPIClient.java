830195a2996f66c3974f55061667bdd033f9e729
MarsMyth <callumshelton2503@gmail.com>
2026-05-03 16:16:54 +1000

    Setup

---
+++ src/main/java/org/mythic_goose/challenge_series/ChallengeSeriesAPIClient.java
@@ -0,0 +1,10 @@
+package org.mythic_goose.challenge_series;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChallengeSeriesAPIClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
